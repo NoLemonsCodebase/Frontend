f91f83b3e8eecbf75e7c97e30d70ea50a2942a25
dauletisataev <daulet@voyage.finance>
2023-12-13 15:25:25 +0500

    feat: add Google analytics (firebase)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-5LKFJ76994" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-5LKFJ76994');
+        `}
+      </Script>
       <body className={inter.className}>
         <Navbar />
         <a
