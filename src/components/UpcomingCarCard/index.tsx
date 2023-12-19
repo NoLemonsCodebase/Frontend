643d208430f0940e17c01b25840f301a2fed07c6
dauletisataev <daulet@voyage.finance>
2023-12-19 18:10:38 +0500

    fix: changed color

--- src/components/UpcomingCarCard/index.tsx
+++ src/components/UpcomingCarCard/index.tsx
@@ -56,10 +56,26 @@ const UpcomingCarCard: React.FunctionComponent<ICarCardProps> = ({
       </div>
       <div
         className={cn(
-          "text-white w-full p-2 rounded-b-lg flex items-center justify-center bg-black bg-opacity-50"
+          "text-white w-full p-2 rounded-b-lg flex items-center justify-center space-x-2 bg-black bg-opacity-50"
         )}
       >
-        Ask details
+        <p>Get early access</p>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="ml-2 icon icon-tabler icon-tabler-brand-whatsapp"
+          width="20"
+          height="20"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="currentColor"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" />
+          <path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1" />
+        </svg>
       </div>
     </div>
   );
