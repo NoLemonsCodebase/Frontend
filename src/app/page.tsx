b2c5d5b50225880f5ce2bd14bea9bee439413cb5
dauletisataev <daulet@voyage.finance>
2023-12-27 19:48:33 +0500

    fix: sorting on client side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,21 +15,5 @@ export default async function Home() {
 
   const data = await res.json();
 
-  const auctionItems = data
-    .filter((item: any) => item.auction && item.auction.length > 0)
-    .sort(
-      (a: any, b: any) =>
-        new Date(b.auction[0].time_ending).getTime() -
-        new Date(a.auction[0].time_ending).getTime()
-    );
-
-  // Filter items without an auction
-  const nonAuctionItems = data.filter(
-    (item: any) => !item.auction || item.auction.length === 0
-  );
-
-  // Combine the two lists
-  const sortedData = [...auctionItems, ...nonAuctionItems];
-
-  return <CarsPage cars={sortedData} />;
+  return <CarsPage cars={data} />;
 }
