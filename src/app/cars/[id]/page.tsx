706b083e73fc922dfe3b76dd8199649dc9b23a14
dauletisataev <daulet@voyage.finance>
2023-12-28 12:46:36 +0500

    feat: dynamic metadata for car detail pages

--- src/app/cars/[id]/page.tsx
+++ src/app/cars/[id]/page.tsx
@@ -2,14 +2,27 @@ import CarDetailPage from "@/components/pages/CarDetailPage";
 import { Metadata } from "next";
 import * as React from "react";
 
-export const metadata: Metadata = {
-  title: "NoLemons.ae - The online auction for car people, by car people",
-  description: "1996 Toyota Land Cruiser VX",
-  openGraph: {
-    images: ["/images/land-cruiser/social.jpeg"],
-  },
-  metadataBase: new URL("https://nolemons.co"),
+type Props = {
+  params: { id: string };
 };
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const res = await fetch(
+    `https://nolemons2.onrender.com/api/v2/cars/${params.id}/`,
+    { next: { revalidate: 0 } }
+  );
+
+  const data = await res.json();
+
+  return {
+    title: "NoLemons.ae - The online auction for car people, by car people",
+    description: `${data.year} ${data.title}`,
+    openGraph: {
+      images: [data.main_image],
+    },
+    metadataBase: new URL("https://nolemons.co"),
+  };
+}
 
 const CarPage: React.FunctionComponent<{ params: any }> = async ({
   params,
