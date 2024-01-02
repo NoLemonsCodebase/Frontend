5dd014a2f4cb198ea974ae47442337f0722467ed
dauletisataev <daulet@voyage.finance>
2024-01-02 19:40:05 +0500

    feat: temp redirect for cayman-gt4

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ["nolemons.ae", "i.postimg.cc"],
   },
+  redirects: async () => {
+    return [
+      {
+        source: "/cayman-gt4",
+        destination: "/cars/22",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
