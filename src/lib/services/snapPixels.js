0c73f30bf24575e8bc75ace0ad5882bf59a63b90
dauletisataev <daulet@voyage.finance>
2024-01-05 14:06:44 +0600

    feat: init snap pixels

--- src/lib/services/snapPixels.js
+++ src/lib/services/snapPixels.js
@@ -2,9 +2,29 @@ function trackWithData(type, data) {
   snaptr("track", type, data);
 }
 
-export const TrackBidViaWA = (phone) => {
+async function sha256(message) {
+  // encode as UTF-8
+  const msgBuffer = new TextEncoder().encode(message);
+
+  // hash the message
+  const hashBuffer = await crypto.subtle.digest("SHA-256", msgBuffer);
+
+  // convert ArrayBuffer to Array
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+  // convert bytes to hex string
+  const hashHex = hashArray
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+  return hashHex;
+}
+
+export const TrackBidViaWA = async (phone) => {
   try {
-    trackWithData("BID_VIA_WA", { user_phone_number: phone });
+    trackWithData("ADD_CART", {
+      user_phone_number: phone,
+      user_hashed_email: await sha256(phone),
+    });
   } catch (e) {
     console.error(e);
   }
