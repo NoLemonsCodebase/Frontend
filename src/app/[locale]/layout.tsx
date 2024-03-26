00efc6908f332770458556dbd1f19efb4bf24553
Dinmukhamed Junussov <dimash.junusov@gmail.com>
2024-03-26 07:31:31 +0500

    added banner

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -18,6 +18,29 @@ export const metadata: Metadata = {
   metadataBase: new URL("https://nolemons.co/"),
 };
 
+function TruckIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M5 18H3c-.6 0-1-.4-1-1V7c0-.6.4-1 1-1h10c.6 0 1 .4 1 1v11" />
+      <path d="M14 9h4l4 4v4c0 .6-.4 1-1 1h-2" />
+      <circle cx="7" cy="18" r="2" />
+      <path d="M15 18H9" />
+      <circle cx="17" cy="18" r="2" />
+    </svg>
+  )
+}
+
 export default function LocaleLayout({ children, params: { locale } }: any) {
   const messages = useMessages();
   const direction = useTextDirection(locale);
@@ -60,7 +83,17 @@ export default function LocaleLayout({ children, params: { locale } }: any) {
       </Script>
       <body className={inter.className}>
         <NextIntlClientProvider locale={locale} messages={messages}>
-          <Banner />
+          {/* <Banner /> */}
+          <div className="bg-yellow-50">
+            <div className="py-2 md:py-3 lg:py-4">
+              <div className="container flex items-center justify-center px-4 md:px-6">
+                <div className="flex items-center space-x-4 text-yellow-600 dark:text-yellow-400">
+                  <TruckIcon className="h-4 w-4" />
+                  <span className="text-sm font-semibold">Free car shipping on first delivery</span>
+                </div>
+              </div>
+            </div>
+          </div>
           <Navbar />
           <ChatWithUsBtn />
           {children}
@@ -69,3 +102,4 @@ export default function LocaleLayout({ children, params: { locale } }: any) {
     </html>
   );
 }
+
