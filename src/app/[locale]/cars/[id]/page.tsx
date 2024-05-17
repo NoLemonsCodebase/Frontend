82713a30426d847e874e53295f995d814e340878
Dinmukhamed Junussov <dimash.junusov@gmail.com>
2024-05-17 16:34:56 +0500

    adde url route in meta data

--- src/app/[locale]/cars/[id]/page.tsx
+++ src/app/[locale]/cars/[id]/page.tsx
@@ -57,7 +57,11 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
       openGraph: {
         title: "NoLemons Online Auction",
         description: carDescription,
-        images: [data.main_image,],
+        images: [{
+          url: data.main_image,
+          width: 800,
+          height: 600,
+        },],
       },
       alternates: {
         canonical: `/cars/${data.url_route}`,
