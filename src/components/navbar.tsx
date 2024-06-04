7e96f161d0017e69993f925e29cc170565b7e5af
Dinmukhamed Junussov <dimash.junusov@gmail.com>
2024-06-04 10:03:28 +0500

    WIP for navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -12,68 +12,111 @@ export function Navbar() {
   const { width } = useWindowSize();
   const t = useTranslations("default.navigation");
   return (
-    <nav className="sticky top-0 z-[11] flex items-center justify-between px-4 md:px-16 py-4 bg-white  shadow">
-      <div className="flex items-center w-full">
-        <a href="/">
+    <nav className="bg-white border-gray-200">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      {/* <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+      </a> */}
+      <a href="/">
           <Image src="/logo.png" width={90} height={16} alt="logo" />
-        </a>
-        {width! > 640 ? (
-          <div className="flex items-center space-x-4">
-            <div className="border-l border-gray-300 h-5 mx-4" />
-            <div className="flex items-center space-x-4">
-              <a
-                className="text-sm md:text-base text-gray-700  hover:bg-gray-200  rounded-md px-2 py-1"
-                href="https://nolemons.co/p/about-us-2"
-              >
-                About us
-              </a>
-              <a
-                className="text-sm md:text-base text-gray-700  hover:bg-gray-200  rounded-md px-2 py-1"
-                href="/how-it-works"
-              >
-                How it works?
-              </a>
-              <a
-                className="text-sm md:text-base text-gray-700  hover:bg-gray-200  rounded-md px-2 py-1"
-                href="/sell-your-car"
-              >
-                Sell your car
-              </a>
-              <a
-                className="text-sm md:text-base text-gray-700  hover:bg-gray-200  rounded-md px-2 py-1"
-                href="/why-us"
-              >
-                Why us?
-              </a>
-              <a
-                className="text-sm md:text-base text-gray-700  hover:bg-gray-200  rounded-md px-2 py-1"
-                href="https://nolemons.co/p/buyer-guide-local"
-              >
-                UAE buyer guide
-              </a>
-              <a
-                className="text-sm md:text-base text-gray-700  hover:bg-gray-200  rounded-md px-2 py-1"
-                href="https://nolemons.co/p/buyer-guide"
-              >
-                International buyer guide
-              </a> 
-            </div>
-          </div>
-        ) : (
-          <DropDownMenu
-            items={[
-              { label: "About us", link: "https://nolemons.co/p/about-us-2" },
-              { label: "How it works?", link: "/how-it-works" },
-              { label: "Sell your car", link: "/sell-your-car" },
-              { label: "Why us?", link: "/why-us" },
-              { label: "UAE buyer guide", link: "https://nolemons.co/p/buyer-guide-local" },
-              { label: "International buyer guide", link: "https://nolemons.co/p/buyer-guide" },
-            ]}
-            withBackground={false}
-            className="ml-auto"
-          />
-        )}
+      </a>
+      <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sell a Car</button>
+          <button data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+            </svg>
+        </button>
+      </div>
+      <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
+        <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <li>
+            <a href="#" className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500" aria-current="page">About us</a>
+          </li>
+          <li>
+            <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">How it works?</a>
+          </li>
+          <li>
+            <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Sell your car</a>
+          </li>
+          <li>
+            <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Why us?</a>
+          </li>
+          <li>
+            <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">UAE buyer guide</a>
+          </li>
+          <li>
+            <a href="#" className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">International buyer guide</a>
+          </li>                    
+        </ul>
+      </div>
       </div>
     </nav>
+
+    // <nav classNameName="sticky top-0 z-[11] flex items-center justify-between px-4 md:px-16 py-4 bg-white  shadow">
+    //   <div classNameName="flex items-center w-full">
+    //     <a href="/">
+    //       <Image src="/logo.png" width={90} height={16} alt="logo" />
+    //     </a>
+    //     {width! > 640 ? (
+    //       <div classNameName="flex items-center space-x-4">
+    //         <div classNameName="border-l border-gray-300 h-5 mx-4" />
+    //         <div classNameName="flex items-center space-x-4">
+    //           <a
+    //             classNameName="text-sm md:text-base text-gray-700  hover:bg-gray-200  rounded-md px-2 py-1"
+    //             href="https://nolemons.co/p/about-us-2"
+    //           >
+    //             About us
+    //           </a>
+    //           <a
+    //             classNameName="text-sm md:text-base text-gray-700  hover:bg-gray-200  rounded-md px-2 py-1"
+    //             href="/how-it-works"
+    //           >
+    //             How it works?
+    //           </a>
+    //           <a
+    //             classNameName="text-sm md:text-base text-gray-700  hover:bg-gray-200  rounded-md px-2 py-1"
+    //             href="/sell-your-car"
+    //           >
+    //             Sell your car
+    //           </a>
+    //           <a
+    //             classNameName="text-sm md:text-base text-gray-700  hover:bg-gray-200  rounded-md px-2 py-1"
+    //             href="/why-us"
+    //           >
+    //             Why us?
+    //           </a>
+    //           <a
+    //             classNameName="text-sm md:text-base text-gray-700  hover:bg-gray-200  rounded-md px-2 py-1"
+    //             href="https://nolemons.co/p/buyer-guide-local"
+    //           >
+    //             UAE buyer guide
+    //           </a>
+    //           <a
+    //             classNameName="text-sm md:text-base text-gray-700  hover:bg-gray-200  rounded-md px-2 py-1"
+    //             href="https://nolemons.co/p/buyer-guide"
+    //           >
+    //             International buyer guide
+    //           </a> 
+    //         </div>
+    //       </div>
+    //     ) : (
+    //       <DropDownMenu
+    //         items={[
+    //           { label: "About us", link: "https://nolemons.co/p/about-us-2" },
+    //           { label: "How it works?", link: "/how-it-works" },
+    //           { label: "Sell your car", link: "/sell-your-car" },
+    //           { label: "Why us?", link: "/why-us" },
+    //           { label: "UAE buyer guide", link: "https://nolemons.co/p/buyer-guide-local" },
+    //           { label: "International buyer guide", link: "https://nolemons.co/p/buyer-guide" },
+    //         ]}
+    //         withBackground={false}
+    //         classNameName="ml-auto"
+    //       />
+    //     )}
+    //   </div>
+    // </nav>
   );
 }
