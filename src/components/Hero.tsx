3ce055c6fdcd158a0734c8fd2104a096504d9b51
Dinmukhamed Junussov <dimash.junusov@gmail.com>
2024-06-04 09:23:58 +0500

    added link to buyer protection

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section className="bg-white">
         <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
-            <a href="https://www.nolemons.co/en/p/about-us-2" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full hover:bg-gray-200" role="alert">
+            <a href="https://www.nolemons.co/en/p/buyer-protection" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full hover:bg-gray-200" role="alert">
                 <span className="text-xs bg-primary-600 rounded-full text-black px-4 py-1.5 mr-3">
                   <Image src="/images/protected.png" alt="Protected" width="30" height="30"/> </span> 
                 <span className="text-sm font-small">Full Buyer Protection</span> 
