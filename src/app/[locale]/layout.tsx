b811d052fb83494ab8a344993dd1d8c1751568fb
Mohamedate <mohame.datefh49@gmail.com>
2024-08-26 17:57:11 +0200

    add time ending with status=live

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -47,17 +47,6 @@ export default function LocaleLayout({ children, params: { locale } }: any) {
         `}
       </Script>
 
-      {/* Microsoft Clarity
-      <Script id="clarity" strategy="afterInteractive">
-        {`
-          (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "no5iqs9494");
-        `}
-      </Script> */}
-
       {/* google tag manager */}
       <Script id="gtm" strategy="afterInteractive">
         {`
