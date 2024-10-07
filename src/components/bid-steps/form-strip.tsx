4874648fe3e7d05f7dcb6052f9f9477b6b6d5ad9
Mohamedate <mohame.datefh49@gmail.com>
2024-10-07 19:29:18 +0200

    init strip

--- src/components/bid-steps/form-strip.tsx
+++ src/components/bid-steps/form-strip.tsx
@@ -1,11 +1,39 @@
-import { Elements } from "@stripe/react-stripe-js";
+import { useCallback, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
+import {
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout,
+  PaymentElement,
+  Elements,
+} from "@stripe/react-stripe-js";
 
-if (process.env.NEXT_PUBLIC_STRIP_PUBLIC_KEY == undefined) {
-  throw new Error("NEXT_PUBLIC_STRIP_PUBLIC_KEY is not defined!!");
-}
+// if (process.env.NEXT_PUBLIC_STRIP_PUBLIC_KEY == undefined) {
+//   throw new Error("process.env.NEXT_PUBLIC_STRIP_PUBLIC_KEY is undefiend");
+// }
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIP_PUBLIC_KEY);
-export default function FormStrip() {
-  return <div>FormStrip</div>;
-}
+const stripePromise = loadStripe(
+  "pk_test_51Q7INO08IA0uLya05iW6lVNwyxY4ePIUNK845DNtOmvRJzaBpRNdBzGcy1LekyBqitcGDbIpaNRot1ICEouc0kx900IRKDPZ84"
+);
+
+const FormStrip = () => {
+  const fetchClientSecret = useCallback(async () => {
+    // Create a Checkout Session
+    const res = await fetch("/api/create-checkout-session", {
+      method: "POST",
+    });
+    const data = await res.json();
+    return data.clientSecret;
+  }, []);
+
+  const options = { fetchClientSecret };
+
+  return (
+    <div id="checkout">
+      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+        <EmbeddedCheckout />
+      </EmbeddedCheckoutProvider>
+    </div>
+  );
+};
+
+export default FormStrip;
