4b2f922cf9e61274b0f2e45fbdd7537a02ad8292
Mohamedate <mohame.datefh49@gmail.com>
2024-10-15 13:08:42 +0200

    add secret key strip

--- src/app/api/checkout-session/route.ts
+++ src/app/api/checkout-session/route.ts
@@ -1,7 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-const stripe = require("stripe")(
-  "sk_test_51JL5IsK7Uh3dA2avnQYplHSxoBlHLk8U8iig7OgXtjjvfZb1NjqdQUhzIf9dpLpm0Yx7DQKK9duoyV7Cee85LOAp003b0Gyb4p"
-);
+const stripe = require("stripe")(process.env.STRIP_SECRET_KEY);
 
 export async function POST(req: NextRequest) {
   try {
