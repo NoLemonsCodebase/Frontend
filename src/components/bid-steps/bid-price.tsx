065b4e8e2f5880b9137fef3e89d3ac30ad2cb3df
Mohamedate <mohame.datefh49@gmail.com>
2024-10-21 15:32:42 +0200

    fix error 2

--- src/components/bid-steps/bid-price.tsx
+++ src/components/bid-steps/bid-price.tsx
@@ -30,103 +30,102 @@ const BidPrice: React.FC<CheckOutStripProps> = ({
     buyers_fee,
   } = carDetail;
 
-  return (
-    <Elements
-      stripe={stripePromise}
-      options={{
-        mode: "payment",
-        amount: convertToSubCurrency(sale_price),
-        currency: currency.toLowerCase(),
-      }}
-    >
-      <CheckOutForm amount={sale_price} currency={currency} />
-    </Elements>
-  );
+  return null;
+  // <Elements
+  //   stripe={stripePromise}
+  //   options={{
+  //     mode: "payment",
+  //     amount: convertToSubCurrency(sale_price),
+  //     currency: currency.toLowerCase(),
+  //   }}
+  // >
+  //   {/* <CheckOutForm amount={sale_price} currency={currency} /> */}
+  // </Elements>
 
   // return <p>sdf</p>;
 };
 
 export default BidPrice;
 
-function CheckOutForm({
-  amount,
-  currency,
-}: {
-  amount: number;
-  currency: string;
-}) {
-  const pathname = usePathname();
-  const strip = useStripe();
-  const elements = useElements();
-  const [error, setError] = useState<string | undefined>("");
-  const [clientSecret, setClientSecret] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(false);
+// function CheckOutForm({
+//   amount,
+//   currency,
+// }: {
+//   amount: number;
+//   currency: string;
+// }) {
+//   const pathname = usePathname();
+//   const strip = useStripe();
+//   const elements = useElements();
+//   const [error, setError] = useState<string | undefined>("");
+//   const [clientSecret, setClientSecret] = useState<string>("");
+//   const [loading, setLoading] = useState<boolean>(false);
 
-  useEffect(() => {
-    async function fetchClientSecret() {
-      const res = await fetch("/api/create-payment-intent", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json", // Added Content-Type header
-        },
-        body: JSON.stringify({
-          amount: convertToSubCurrency(amount),
-          currency,
-        }),
-      });
-      const data = await res.json();
+//   useEffect(() => {
+//     async function fetchClientSecret() {
+//       const res = await fetch("/api/create-payment-intent", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json", // Added Content-Type header
+//         },
+//         body: JSON.stringify({
+//           amount: convertToSubCurrency(amount),
+//           currency,
+//         }),
+//       });
+//       const data = await res.json();
 
-      setClientSecret(data.clientSecret);
-    }
+//       setClientSecret(data.clientSecret);
+//     }
 
-    fetchClientSecret();
-  }, []);
+//     fetchClientSecret();
+//   }, []);
 
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    setLoading(true);
+//   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+//     e.preventDefault();
+//     setLoading(true);
 
-    if (!strip || !elements) return;
+//     if (!strip || !elements) return;
 
-    const { error: submitError } = await elements.submit();
+//     const { error: submitError } = await elements.submit();
 
-    if (submitError) {
-      setError(submitError.message);
-      setLoading(false);
-      return;
-    }
+//     if (submitError) {
+//       setError(submitError.message);
+//       setLoading(false);
+//       return;
+//     }
 
-    const { error } = await strip.confirmPayment({
-      elements,
-      clientSecret,
-      confirmParams: {
-        return_url: "https://nolemons-dev.vercel.app/en/payment-success",
-      },
-    });
+//     const { error } = await strip.confirmPayment({
+//       elements,
+//       clientSecret,
+//       confirmParams: {
+//         return_url: "https://nolemons-dev.vercel.app/en/payment-success",
+//       },
+//     });
 
-    if (error) {
-      setError(error.message);
-    }
+//     if (error) {
+//       setError(error.message);
+//     }
 
-    setLoading(false);
-  }
+//     setLoading(false);
+//   }
 
-  if (!clientSecret || !strip || !elements) {
-    return <p className=" my-4">loading...</p>;
-  }
+//   if (!clientSecret || !strip || !elements) {
+//     return <p className=" my-4">loading...</p>;
+//   }
 
-  return (
-    <form onSubmit={handleSubmit} className=" max-w-sm mt-10">
-      {clientSecret && <PaymentElement />}
-      {error && <div>{error}</div>}
-      <button
-        disabled={!strip || loading}
-        className=" bg-green-600 w-full text-white font-semibold py-3 rounded-md mt-4"
-      >
-        {!loading
-          ? `Pay ${amount.toLocaleString()} ${currency}`
-          : "processing..."}
-      </button>
-    </form>
-  );
-}
+//   return (
+//     <form onSubmit={handleSubmit} className=" max-w-sm mt-10">
+//       {clientSecret && <PaymentElement />}
+//       {error && <div>{error}</div>}
+//       <button
+//         disabled={!strip || loading}
+//         className=" bg-green-600 w-full text-white font-semibold py-3 rounded-md mt-4"
+//       >
+//         {!loading
+//           ? `Pay ${amount.toLocaleString()} ${currency}`
+//           : "processing..."}
+//       </button>
+//     </form>
+//   );
+// }
