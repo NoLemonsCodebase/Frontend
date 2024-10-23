5156a68a5a28e0f7934e491c16d13a21998c0ab0
Mohmed Atef <88455397+Mohamedate@users.noreply.github.com>
2024-10-23 15:48:00 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,28 +4,10 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 
-const domain_images = [
-  "cdn.elferspot.com",
-  "www.elferspot.com",
-  "www.builtforbackroads.com",
-  "www.kickdown.com",
-  "nolemons.ae",
-  "nolemons.co",
-  "i.postimg.cc",
-  "nolemons.s3.amazonaws.com",
-  "nolemons.s3.us-west-2.amazonaws.com",
-  "api.typeform.com",
-  "www.pcarmarket.com",
-  "flagcdn.com",
-];
 
 const nextConfig = {
   images: {
-    remotePatterns: domain_images.map((domain) => ({
-      protocol: "https",
-      hostname: domain,
-      pathname: "**",
-    })),
+    remotePatterns: [{ protocol: "https", hostname: "**", pathname: "**" }],
   },
 };
 
